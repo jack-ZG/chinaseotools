f1a4d1aca3406d84c05a891d3336de3a920a6529
xiaohaige <2268713435@qq.com>
2017-06-06 09:52:07 +0800

    12312

--- word.py
+++ word.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
 #codeing:utf-8
+import requests
+from bs4 import BeautifulSoup
 
-import openpyxl
+def baidu_index(word="华锐视点",domain="www.vrnew.com"):
+	headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"}
+	payload = {'wd': word}
+	req=requests.get("https://www.baidu.com/s",params=payload,headers=headers)
+	soup=BeautifulSoup(req.text,'lxml')
+	for i in soup.select(".c-container "):
+		print(i['id'])
+		print(i.get_text())
+		
+
+
+def main():
+	baidu_index()
+
+
+if __name__ == '__main__':
+	main()
