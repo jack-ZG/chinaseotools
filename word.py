a73eddab386219519d0f7e22cf2c5b379d7033df
xiaohaige <2268713435@qq.com>
2017-06-08 10:54:53 +0800

    123123

--- word.py
+++ word.py
@@ -3,10 +3,32 @@
 import os
 import openpyxl as excel
 
-os.chdir(str(os.getcwd())+"\wordlist")
-wb=excel.load_workbook(os.listdir()[0])
-print(wb.get_sheet_names())
-ws=wb[wb.get_sheet_names()[0]]
-data=ws["A"]
-for i in data:
-	print(i.value)
+
+
+def get_wordlist():
+	"""
+	文件结尾必须以：xlsx结尾 切放到wordlist文件夹下
+	"""
+	wordlist=[]
+	os.chdir(str(os.getcwd())+"\wordlist")
+	for file in os.listdir():
+		if os.path.isfile(file) and str(file).endswith(".xlsx",):
+			wb=wb=excel.load_workbook(file)
+			ws=wb[wb.get_sheet_names()[0]]
+			data=ws["A"]
+			for i in data:
+				wordlist.append(i.value)
+	return list(set(wordlist))
+def get_relation_bool():
+	""""""
+	relwords=[]
+	return relwords
+
+def main():
+	wordlist=get_wordlist()
+	for i in wordlist:
+		print(i)
+
+if __name__ == '__main__':
+	main()
+
